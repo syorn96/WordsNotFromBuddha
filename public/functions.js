261a174e55a9b876bdd2b8c6bf0ffd19977cdd35
syorn96 <syorn2014@gmail.com>
2022-09-16 11:12:25 -0700

    adds scroll to top button for meditate

--- public/functions.js
+++
@@ -1,7 +0,0 @@
-// let buttons = document.querySelectorAll('.clickme')
-
-// buttons.forEach(e => {
-//     e.addEventListener('click', ()=> {
-//         e.parentElement.setAttribute('style', 'display:none;')
-//     })
-// })
--- users/users/public/functions.js
+++ users/users/public/functions.js
@@ -1,7 +1,25 @@
 let buttons = document.querySelectorAll('.clickme')
 
+// When user clicks button on meditate page, disappear for better UX.
+let mybutton = document.getElementById("top-Btn");
 buttons.forEach(e => {
     e.addEventListener('click', ()=> {
         e.parentElement.setAttribute('style', 'display:none;')
     })
 })
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
