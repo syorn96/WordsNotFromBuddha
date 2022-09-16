55b8aa7a59b86eeb118e848f2d1b2aa4045ea949
syorn96 <syorn2014@gmail.com>
2022-09-16 11:29:18 -0700

    more css

--- users/users/public/functions.js
+++ users/users/public/functions.js
@@ -1,21 +1,25 @@
+// Button variables
 let buttons = document.querySelectorAll('.clickme')
+let mybutton = document.getElementById("top-Btn");
+let reflectbutton = document.getElementById("reflect-Btn");
 
 // When user clicks button on meditate page, disappear for better UX.
-let mybutton = document.getElementById("top-Btn");
 buttons.forEach(e => {
     e.addEventListener('click', ()=> {
         e.parentElement.setAttribute('style', 'display:none;')
     })
 })
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, show the hidden buttons
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
+    reflectbutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
+    reflectbutton.style.display = "none";
   }
 }
 
