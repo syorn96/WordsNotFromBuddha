375c70c158c7192267e2700d4083bedeaac6c709
syorn96 <syorn2014@gmail.com>
2022-09-16 18:09:04 -0700

    adds toggle button yin/yang mdoe

--- users/users/public/functions.js
+++ users/users/public/functions.js
@@ -1,8 +1,12 @@
 // Button variables
 let buttons = document.querySelectorAll('.clickme')
-let mybutton = document.getElementById("top-Btn");
-let reflectbutton = document.getElementById("reflect-Btn");
-
+let myButton = document.getElementById("top-Btn");
+let reflectButton = document.getElementById("reflect-Btn");
+let yinBtn = document.getElementById("yin");
+let yangBtn = document.getElementById("yang");
+let body = document.getElementById("background");
+let headerImage = document.getElementById("header");
+let boxy = document.getElementsByClassName('boxy')
 // When user clicks button on meditate page, disappear for better UX.
 buttons.forEach(e => {
     e.addEventListener('click', ()=> {
@@ -15,11 +19,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-    reflectbutton.style.display = "block";
+    myButton.style.display = "block";
+    relectButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
-    reflectbutton.style.display = "none";
+    myButton.style.display = "none";
+    reflectButton.style.display = "none";
   }
 }
 
@@ -27,3 +31,15 @@ function scrollFunction() {
 function topFunction() {
   document.documentElement.scrollTop = 0;
 }
+
+yangBtn.addEventListener('click', ()=> {
+    headerImage.setAttribute('src','/public/media/WNFB.png')
+    body.setAttribute('style','background-image: url("https://cdn.pixabay.com/photo/2016/10/19/03/32/duck-1752065_960_720.jpg"); color:black;')
+    boxy.setAttribute('style','background-color: rgba(255, 255, 255, 0.485);')
+})
+
+yinBtn.addEventListener('click', ()=> {
+    headerImage.setAttribute('src','/public/media/WNFB2(white).png')
+    body.setAttribute('style','background-image: url("https://cdn.pixabay.com/photo/2016/09/08/12/00/stars-1654074_960_720.jpg"); color: white;')
+    boxy.setAttribute('style','background-color: rgba(0, 0, 0, 0.485);')
+})
