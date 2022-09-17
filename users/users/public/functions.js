972e7c1c7f40d268bedb1a2a119a11be92a46d06
syorn96 <syorn2014@gmail.com>
2022-09-16 18:36:04 -0700

    divs now toggle color on click

--- users/users/public/functions.js
+++ users/users/public/functions.js
@@ -6,7 +6,7 @@ let yinBtn = document.getElementById("yin");
 let yangBtn = document.getElementById("yang");
 let body = document.getElementById("background");
 let headerImage = document.getElementById("header");
-let boxy = document.getElementsByClassName('boxy')
+let boxy = document.querySelectorAll(".boxy")
 // When user clicks button on meditate page, disappear for better UX.
 buttons.forEach(e => {
     e.addEventListener('click', ()=> {
@@ -35,11 +35,15 @@ function topFunction() {
 yangBtn.addEventListener('click', ()=> {
     headerImage.setAttribute('src','/public/media/WNFB.png')
     body.setAttribute('style','background-image: url("https://cdn.pixabay.com/photo/2016/10/19/03/32/duck-1752065_960_720.jpg"); color:black;')
-    boxy.setAttribute('style','background-color: rgba(255, 255, 255, 0.485);')
+    boxy.forEach(e=> {
+        e.setAttribute('style','background-color: rgba(255, 255, 255, 0.485);')
+    })
 })
 
 yinBtn.addEventListener('click', ()=> {
     headerImage.setAttribute('src','/public/media/WNFB2(white).png')
     body.setAttribute('style','background-image: url("https://cdn.pixabay.com/photo/2016/09/08/12/00/stars-1654074_960_720.jpg"); color: white;')
-    boxy.setAttribute('style','background-color: rgba(0, 0, 0, 0.485);')
+    boxy.forEach(e=> {
+        e.setAttribute('style','background-color: rgba(0, 0, 0, 0.485);')
+    })
 })
