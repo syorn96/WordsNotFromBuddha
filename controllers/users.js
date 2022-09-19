5d54211529bfd0329a840f2348cc71f4f488956a
syorn96 <syorn2014@gmail.com>
2022-09-19 10:52:54 -0700

    compare old password, create new password works on profile/account route

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,7 @@ const axios = require('axios')
 const determineSign = require('../public/script')
 const { response } = require('express')
 let sign = null
+const noLoginMessage = 'Incorrect username or password'
 // GET /users/new -- render a form to create a new user
 router.get('/new', (req,res) => {
     res.render('users/new.ejs')
@@ -65,7 +66,6 @@ router.post('/login', async (req,res)=> {
                 email: req.body.email
             }
         })
-        const noLoginMessage = 'Incorrect username or password'
         // if the user is not found -- send the user back to the login form
         if(!user) {
             console.log('user not found')
@@ -105,7 +105,7 @@ router.delete('/profile/account', async (req,res)=> {
         const deleteUser = await db.user.destroy({
                 where: {email: res.locals.user.email}
             })
-    res.redirect('/users/new')
+        res.redirect('/users/new')
 
     }catch(err) {
         console.log(err)
@@ -114,18 +114,26 @@ router.delete('/profile/account', async (req,res)=> {
 })
 
 router.put('/profile/account', async (req,res)=> {
+    const user = await db.user.findOne({
+        where: {email: res.locals.user.email}
+    })
     if(!res.locals.user) {
         res.redirect('/users/login?message=You must authenticate before you are authorized to view this resource.')
         // otherwise, show them their profile
+    } else if(!bcrypt.compareSync(req.body.password, user.password)) {
+        console.log('wrong password')
+        res.redirect('/users/profile/account/?message=' + noLoginMessage)
+    // if the user is found and the supplied password matches what is in DB, -- log them in
     } else {
         try {
+        const hashedPassword = bcrypt.hashSync(req.body.newPassword, 12)
         const editUser = await db.user.update({
                 username: req.body.username,
-                password: req.body.password,
                 birthMonth: req.body.month,
-                birthDay: req.body.day
-            }, {
-                where: {email: res.locals.user.email}
+                birthDay: req.body.day,
+                password: hashedPassword
+            },{
+                where: {id: res.locals.user.id}
             })
     res.redirect('/users/profile')
 
@@ -146,8 +154,11 @@ router.get('/profile/account', async (req,res)=> {
         userInfo = await db.user.findOne({
             where: {
                 email: res.locals.user.email
+            },
+            defaults: {
+                password: res.locals.user.password
             }
-    })
+        })
     res.render('users/account.ejs', { user: userInfo})
 
     }catch(err) {
